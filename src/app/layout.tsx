2b1f2b0c1a7cb1e144286e78c76d0ef141d65f52
Rohit Verma <28vrohit@gmail.com>
2024-12-07 23:20:17 +0530

    /

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ThemeProvider } from "@/lib/ThemeToggle/ThemeProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,16 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
