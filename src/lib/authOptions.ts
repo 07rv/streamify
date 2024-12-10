d780a96c3e61454c18d48d5b33c35bf9f313aeca
Rohit Verma <28vrohit@gmail.com>
2024-12-10 07:12:21 +0530

    ADD KEY

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -74,5 +74,5 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: "WDEFRGHJMHGFDEFGHJMHNGFDEFGHJMJHFDEGHNJMDEFGHDFG",
 };
